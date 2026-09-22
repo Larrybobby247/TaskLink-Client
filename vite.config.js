3995fcabb3d52bf123825163d17ba6d1c455e18e
Lawrence Adebowale <larrybobby247@gmail.com>
2026-09-22 02:07:11 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': { target: 'https://tasklink-58mu.onrender.com/', changeOrigin: true },
     },
   },
 });
